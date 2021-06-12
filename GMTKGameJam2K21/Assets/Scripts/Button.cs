d681e448c1d0ebb320bf77df6807e7b9458a0a53
MonkeyZebraProductions <73033054+MonkeyZebraProductions@users.noreply.github.com>
2021-06-12 16:16:28 +0100

    Button Creation

---
+++ GMTKGameJam2K21/Assets/Scripts/Button.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+
+    public Transform PairedButton;
+    public GameObject SpawnedObject;
+    private bool hasPressed;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer==15 && hasPressed==false)
+        {
+            Instantiate(SpawnedObject, PairedButton.position, Quaternion.identity);
+            hasPressed = true;
+        }
+    }
+}
