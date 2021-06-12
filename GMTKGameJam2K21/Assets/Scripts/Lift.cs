0e781bea36befc600fc7a8d7b2a2544727cc37d9
MonkeyZebraProductions <73033054+MonkeyZebraProductions@users.noreply.github.com>
2021-06-12 13:36:34 +0100

    Lift part 1

---
+++ GMTKGameJam2K21/Assets/Scripts/Lift.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lift : MonoBehaviour
+{
+    public GameObject liftable;
+    private bool canLift;
+    public float BoxSize = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        canLift = Physics2D.OverlapBox(transform.position, new Vector2(BoxSize,BoxSize),0,10);
+        Debug.Log(canLift);
+        if(Input.GetButtonDown("Lift") && canLift)
+        {
+            Debug.Log("Hi");
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, new Vector3(BoxSize, BoxSize, BoxSize));
+    }
+}
--- GMTKGameJam2K21/Assets/Scripts/Shoot.cs
+++ GMTKGameJam2K21/Assets/Scripts/Shoot.cs
@@ -19,7 +19,7 @@ public class Shoot : MonoBehaviour
         
         float angle = (Mathf.Atan2(movement.facedirection.y, movement.facedirection.x) * Mathf.Rad2Deg);
         Debug.Log(movement.movem.x);
-        if(Input.GetKeyDown(KeyCode.Q))
+        if(Input.GetButtonDown("Shoot"))
         {
             Instantiate(Projectile, transform.position, Quaternion.Euler(0,0,angle));
         }
